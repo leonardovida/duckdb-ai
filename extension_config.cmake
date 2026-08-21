d9b8faf085959f743bf4d239fd2660d742fa537a
Leonardo Vida <lleonardovida@gmail.com>
2026-08-21 10:19:14 -0400

    release: prepare duckdb_ai 0.4.20 (#76)

--- extension_config.cmake
+++ extension_config.cmake
@@ -3,7 +3,7 @@
 # Extension from this repo
 duckdb_extension_load(ai
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
-    EXTENSION_VERSION 0.4.19
+    EXTENSION_VERSION 0.4.20
 )
 
 # Any extra extensions that should be built
